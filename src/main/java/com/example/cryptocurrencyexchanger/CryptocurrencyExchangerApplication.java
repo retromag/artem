5a5951bd3664c881ee293675a9e02da38e91c765
Ovcharenko Oleksandr <ovcharenko.messor@gmail.com>
2022-08-04 11:37:04 +0200

    init

---
+++ src/main/java/com/example/cryptocurrencyexchanger/CryptocurrencyExchangerApplication.java
@@ -0,0 +1,13 @@
+package com.example.cryptocurrencyexchanger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptocurrencyExchangerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CryptocurrencyExchangerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cryptocurrencyexchanger/CryptocurrencyExchangerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cryptocurrencyexchanger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptocurrencyExchangerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
