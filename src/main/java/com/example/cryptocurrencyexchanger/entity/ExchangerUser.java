212940f35bc2926370a945bf618f737b12b793ec
Ovcharenko Oleksandr <ovcharenko.messor@gmail.com>
2022-08-04 11:50:51 +0200

    add docker compose file

---
+++ src/main/java/com/example/cryptocurrencyexchanger/entity/ExchangerUser.java
@@ -0,0 +1,14 @@
+package com.example.cryptocurrencyexchanger.entity;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+
+@Getter
+@Setter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+public class ExchangerUser {
+}
