0a3cea664f622858951ce71f6999e811fa82335e
Viktoriia Nakoryk <Viktoriia_Nakoryk@epam.com>
2022-08-12 10:04:58 +0200

    add markup for reviews page

--- src/main/resources/static/js/setRate.js
+++ src/main/resources/static/js/setRate.js
@@ -1,14 +1,8 @@
 const rateElements = document.querySelectorAll('.js-set-rate');
-const rateInput = document.querySelectorAll('.js-value-rate');
-console.log('rateElements', rateElements);
-console.log('rateInput', rateInput);
+const rateInput = document.querySelector('.js-value-rate');
 
 rateElements.forEach((rate) => {
     rate.addEventListener('click', () => {
-        console.log('rate', rate);
-        console.log('rate.getAttribute(\'data-coin-name\')', rate.getAttribute('data-rate'));
-        console.log(rateInput);
-        const value = rate.getAttribute('data-rate');
-        rateInput.value ='hybhjnk';
+        rateInput.value = rate.getAttribute('data-rate');
     })
 })
