de9e60da213f19af7fb28222f2104ad1ba8dbba0
Viktoriia Nakoryk <Viktoriia_Nakoryk@epam.com>
2022-08-14 09:46:56 +0200

    add disabling button when tumbler not checked

---
+++ src/main/resources/static/js/checkButton.js
@@ -0,0 +1,15 @@
+const btnRegister = document.querySelector('.js-btn-registration');
+const tumblers = document.querySelectorAll('.js-tumbler');
+
+tumblers.forEach((tumbler) => {
+    tumbler.addEventListener('change', () => {
+        if (!tumblers[0].checked && !tumblers[1].checked) {
+            btnRegister.disabled = true;
+        } else if (!tumblers[0].checked || !tumblers[1].checked) {
+            btnRegister.disabled = true;
+        } else if (tumblers[0].checked && tumblers[1].checked) {
+            btnRegister.disabled = false;
+        }
+    })
+});
+
