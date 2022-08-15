8d7c71f1c400c1c74f075d5927c814743dd79ba1
Viktoriia Nakoryk <Viktoriia_Nakoryk@epam.com>
2022-08-15 10:21:03 +0200

    add fonts to all pages, fix bug with disabling button

--- src/main/resources/static/js/checkButton.js
+++ src/main/resources/static/js/checkButton.js
@@ -1,6 +1,6 @@
 const btnRegister = document.querySelector('.js-btn-registration');
 const tumblers = document.querySelectorAll('.js-tumbler');
-
+btnRegister.disabled = true;
 tumblers.forEach((tumbler) => {
     tumbler.addEventListener('change', () => {
         if (!tumblers[0].checked && !tumblers[1].checked) {
