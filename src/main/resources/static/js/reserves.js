d95e3b0a252b7911784752e2142ecb9b21df7cbc
Viktoriia Nakoryk <Viktoriia_Nakoryk@epam.com>
2023-02-28 13:27:49 +0100

    fix problem with usdt. clean code

--- src/main/resources/static/js/reserves.js
+++ src/main/resources/static/js/reserves.js
@@ -4,7 +4,8 @@ console.log('works', coinsAmountName);
 coinsAmountName.forEach((coin) => {
     if (coin.textContent.startsWith('UAH') ||
         coin.textContent.startsWith('RUB') ||
-        coin.textContent.startsWith('USD')
+        coin.textContent.startsWith('USD') &&
+        coin.textContent !== 'USDT'
     ) {
         coin.textContent = coin.textContent.substring(0, 3);
     }
